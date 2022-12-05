d461550a4ea3a2ca157dd2de897f408c26ee3b81
AlexLuik001 <64197415+AlexLuik@users.noreply.github.com>
2022-12-05 19:30:25 +0400

    simplexmethod
    Test

---
+++ MatrixMath.pas
@@ -0,0 +1,347 @@
+﻿unit MatrixMath;
+
+interface
+
+type ExtendMatrix = record
+  baseMatrix: array[,] of real;
+  rowX: array of string;
+  columnX: array of string;
+  public
+    constructor Create(
+      baseMatrix: array[,] of real;
+      rowX: array of string;
+      columnX: array of string
+    );
+    begin
+      Self.baseMatrix := baseMatrix;
+      Self.rowX := rowX;
+      Self.columnX := columnX;
+    end;
+    
+    function FindRowIndex(ColumnIndex: integer): integer;
+    function FindColumnIndex(): integer;
+    function RowElementsSuccess(rowIndex: integer): boolean;
+    
+    procedure GaussianElimination(Indexes: (integer, integer));
+    procedure DeleteColumn(index: integer);
+    procedure ChangeRowAndColumn(rowXIndex: integer; columnXIndex: integer);
+    procedure Print;
+    
+end;
+
+
+function findNoZeroElementIndex(matrix: array[,] of real): (integer, integer);
+function GaussianElimination(
+  matrix: array[,] of real;
+  ElementIndexes: (integer, integer)
+): array[,] of real;
+function copyMatrix(matrix: array[,] of real): array[,] of real;
+function rowSum(matrix: array[,] of real; row: integer): real;
+function det(
+  matrix: array[,] of real;
+  topRow: integer;
+  bottomRow: integer;
+  leftColumn: integer;
+  rightColumn: integer
+): real;
+function deleteColumn(matrix: array[,] of real; column: integer): array[,] of real;
+procedure ChangeRow(matrix: array[,] of real; row1, row2: integer; column: integer);
+function rank(matrix: array[,] of real): integer;
+//procedure printAnswer(matrix: ExtendedMatrix);
+
+implementation
+
+function ExtendMatrix.FindColumnIndex(): integer;
+var
+  currentElement: real;
+  maxAbsElement: real := 0.0;
+  absElement: real := 0.0;
+  lastRowIndex: integer := self.baseMatrix.GetLength(0) - 1;
+begin
+  for var i := 1 to self.baseMatrix.GetLength(1) - 1 do
+  begin
+    currentElement := self.baseMatrix[lastRowIndex, i];
+    if (currentElement < 0) then
+    begin
+      absElement := Abs(currentElement);
+      if (absElement > maxAbsElement) then
+      begin  
+        maxAbsElement := absElement;
+        Result := i;
+      end;       
+    end;     
+  end;
+end;
+
+function ExtendMatrix.FindRowIndex(ColumnIndex: integer): integer;
+var
+  currentElement: real := 0;
+  minDivisionResult: real := MaxReal;
+  divisionResult: real := 0;
+begin
+  for var i := 0 to self.baseMatrix.GetLength(0) - 3 do
+  begin
+    currentElement := self.baseMatrix[i, ColumnIndex];
+    if (currentElement <= 0) then
+      continue;
+    
+    divisionResult := self.baseMatrix[i, 0] / currentElement;
+    if (divisionResult < minDivisionResult) then
+    begin
+      minDivisionResult := divisionResult;
+      Result := i;
+    end;
+  end;
+end;
+
+function ExtendMatrix.RowElementsSuccess(rowIndex: integer): boolean;
+begin
+  Result := true;
+  for var i := 1 to self.baseMatrix.GetLength(1) - 1 do
+  begin
+    if (self.baseMatrix[rowIndex, i] < 0) then
+    begin
+      Result := false;
+      break;
+    end;
+  end;
+end;
+
+procedure ExtendMatrix.GaussianElimination(Indexes: (integer, integer));
+var
+  Row, Column: integer;
+  piv: real;
+begin
+  (Row, Column) := Indexes;
+  Column += 1; // первый столбец содержит левую часть уравнения
+  piv := self.baseMatrix[Row, Column];
+  var newMatrix := new real[Length(self.baseMatrix, 0) - 1, Length(self.baseMatrix, 1) - 1];
+  
+  // заполнение всей матрицы значениями 1 / разр-ий эл-т * определитель
+  for var i := 0 to Length(self.baseMatrix, 0) - 1 do
+    for var j := 0 to Length(self.baseMatrix, 1) - 1 do
+      newMatrix[i,j] := det(self.baseMatrix, i, Row, j, Column) / piv;
+
+  // заполненить строки                     
+  for var i := 0 to Length(self.baseMatrix, 1) - 1 do
+    newMatrix[Row, i] := self.baseMatrix[Row, i] / piv;
+  
+  // заполненить столбцы
+  for var i := 0 to Length(self.baseMatrix, 0) - 1 do
+    newMatrix[i, Column] := -self.baseMatrix[i, Column] / piv;
+  
+  // вычисление элемента для разрешающего элемента
+  newMatrix[Row, Column] := 1 / piv;
+  
+  self.baseMatrix := newMatrix;
+end;
+
+procedure ExtendMatrix.DeleteColumn(index: integer);
+begin
+  
+end;
+
+procedure ExtendMatrix.ChangeRowAndColumn(rowXIndex: integer; columnXIndex: integer);
+var
+  newRowValue: string;
+begin
+  newRowValue := '-' + self.columnX[rowXIndex].Substring(0, self.columnX[rowXIndex].Length - 1);  
+  self.columnX[rowXIndex] := self.rowX[columnXIndex].Substring(1) + ' ';
+  self.rowX[columnXIndex] := newRowValue;
+end;
+
+procedure ExtendMatrix.Print;
+var charSpace: integer := 7;
+begin
+  Write('   ':charSpace, '   ', '1':charSpace, '   ');
+  foreach var val in self.rowX do
+    Write(val:charSpace);
+  Writeln('   ');
+  for var i := 0 to self.baseMatrix.GetLength(0) - 1 do
+  begin
+    Write(self.columnX[i]:charSpace, '   ');
+    Write(self.baseMatrix[i, 0]:charSpace:2, '   ');
+    for var j := 1 to Length(self.baseMatrix, 1) - 1 do
+      Write(self.baseMatrix[i, j]:charSpace:2);
+    Writeln();
+  end;
+end;
+
+function findNoZeroElementIndex(matrix: array[,] of real): (integer, integer);
+begin
+  for var i := 0 to Length(matrix, 0) - 1 do
+    for var j := 0 to Length(matrix, 1) - 1 do
+      if (matrix[i,j] <> 0.0) then
+      begin
+        Result := (i, j);
+        exit;
+      end;       
+end;
+
+function gaussianElimination(
+  matrix: array[,] of real;
+  ElementIndexes: (integer, integer)
+): array[,] of real;
+var
+  ElementRow: integer;
+  ElementColumn: integer;
+  Element: real;
+  accuracyNum: real := 0.000001;
+begin
+  Result := new real[Length(matrix, 0), Length(matrix, 1)];
+  (ElementRow, ElementColumn) := ElementIndexes;
+  Element := matrix[ElementRow, ElementColumn];
+  
+  // заполнение всей матрицы значениями 1 / разр. эл-т * определитель
+  for var i := 0 to Length(matrix, 0) - 1 do
+    for var j := 0 to Length(matrix, 1) - 1 do
+      Result[i,j] := det(matrix, i, ElementRow, j, ElementColumn) / Element;
+
+  // заполнение строки                     
+  for var i := 0 to Length(matrix, 1) - 1 do
+    Result[ElementRow, i] := matrix[ElementRow, i] / Element;
+  
+  // заполнение столбца
+  for var i := 0 to Length(matrix, 0) - 1 do
+    Result[i, ElementColumn] := -matrix[i, ElementColumn] / Element;
+  
+  // вычисление элемента на месте разрешающего элемента
+  Result[ElementRow, ElementColumn] := 1 / Element;
+  
+  // округление
+  for var i := 0 to Result.GetLength(0) - 1 do
+    for var j := 0 to Result.GetLength(1) - 1 do
+      if ((Result[i, j] < accuracyNum) and (Result[i, j] > -accuracyNum)) then
+        Result[i, j] := 0;
+end;
+
+function copyMatrix(matrix: array[,] of real): array[,] of real;
+var
+  rowsAmount := matrix.GetLength(0);
+  columnsAmount := matrix.GetLength(1);
+begin
+  Result := new real[rowsAmount, columnsAmount];
+  for var i := 0 to rowsAmount - 1 do
+    for var j := 0 to columnsAmount - 1 do
+      Result[i,j] := matrix[i,j];
+end;
+
+function rowSum(matrix: array[,] of real; row: integer): real;
+begin
+  Result := 0;
+  for var i := 0 to matrix.GetLength(1) - 1 do
+    Result += matrix[row, i];
+end;
+
+function det(
+  matrix: array[,] of real;
+  topRow: integer;
+  bottomRow: integer;
+  leftColumn: integer;
+  rightColumn: integer
+): real;
+begin
+  Result := matrix[topRow, leftColumn] * matrix[bottomRow, rightColumn] -
+            matrix[topRow, rightColumn] * matrix[bottomRow, leftColumn];         
+end;
+
+function deleteColumn(matrix: array[,] of real; column: integer): array[,] of real;
+begin
+  Result := new real[matrix.GetLength(0), matrix.GetLength(1) - 1];
+  
+  for var i := 0 to matrix.GetLength(0) - 1 do
+    for var j := 0 to matrix.GetLength(1) - 2 do
+      if (j >= column) then
+        Result[i, j] := matrix[i, j + 1]
+      else
+        Result[i, j] := matrix[i, j];
+end;
+
+procedure ChangeRow(matrix: array[,] of real; row1, row2: integer; column: integer);
+var
+  temp: real;
+begin
+  for var i := 0 to column do
+  begin
+    temp := matrix[row1,i];
+    matrix[row1,i] := matrix[row2,i];
+    matrix[row2,i] := temp;
+  end;
+end;
+
+function rank(matrix: array[,] of real): integer;
+var
+  rowsAmount := matrix.GetLength(0);
+  columnsAmount := matrix.GetLength(1);
+begin
+  var rank := Min(rowsAmount, columnsAmount);
+  
+  var row := 0;
+  while (row < rank) do
+  begin
+    // Диагональный элемент не равен 0
+    if (matrix[row,row] <> 0) then
+    begin
+      for var col := 0 to rowsAmount - 1 do
+        if (col <> row) then
+        begin
+          var mult := matrix[col,row] / matrix[row,row];
+          for var i := 0 to rank do
+            matrix[col,i] -= mult * matrix[row,i];
+        end;
+        
+      row += 1;
+    end
+    else
+    begin
+      var reduce := true;
+      
+      for var i := row + 1 to rowsAmount - 1 do
+        if (matrix[i,row] <> 0) then
+        begin
+          ChangeRow(matrix, row, i, rank);
+          reduce := false;
+          break;
+        end;
+        
+      if (reduce) then
+      begin
+        rank -= 1;
+        
+        for var i := 0 to rowsAmount - 1 do
+          matrix[i,row] := matrix[i,rank];
+      end;
+    end
+  end;
+  
+  Result := rank;
+end;
+
+{procedure printAnswer(matrix: ExtendedMatrix);
+var
+  currentSymbolAscii := 97;
+  rootSymbols: array of char;
+begin
+  rootSymbols := new char[matrix.rowX.Length];
+  for var i := 0 to matrix.rowX.Length - 1 do
+  begin
+    rootSymbols[i] := char(currentSymbolAscii + i);
+    Writeln(matrix.rowX[i].Substring(1), '= ', rootSymbols[i]);
+  end;
+  for var i := 0 to matrix.columnX.Length - 1 do
+    if (matrix.columnX[i] <> '0=') then
+    begin
+      Write(matrix.columnX[i], ' ', matrix.baseMatrix[i, 0], ' ');
+      for var j := 1 to matrix.baseMatrix.GetLength(1) - 1 do
+      begin
+        var matrixElement := -matrix.baseMatrix[i, j];
+        if (matrixElement = -1) then
+          Write('-')
+        else
+          Write(matrixElement);
+        Write(rootSymbols[j - 1], ' ');
+      end;
+      Writeln();
+    end;
+end;} 
+end.
---
+++ simplexmethod.pas
@@ -0,0 +1,201 @@
+﻿uses Crt;
+//uses System;
+uses MatrixMath;
+
+const
+  matrixRowsAmount: integer = 3;
+  matrixColumnsAmount: integer = 4;
+
+var
+  RowIndex: integer;
+  ColumnIndex: integer;
+  //matrix: array[,] of real;
+  //resultMatrix: array[,] of real;
+  extendResultMatrix: ExtendMatrix;
+  tempMatrix: array[,] of real;
+  FuncCoff: array of real;
+  
+  matrixTransformCount: integer := 1;
+  {matrixExample1 := new real[5, 6] (
+    (  3.0,  1.0, -4.0,  2.0, -5.0,  9.0),
+    (  6.0,  0.0,  1.0, -3.0,  4.0, -5.0),
+    (  1.0,  0.0,  1.0, -1.0,  1.0, -1.0),
+    (  0.0,  2.0,  6.0, -5.0,  1.0,  4.0),
+    (-10.0, -1.0,  2.0,  2.0,  0.0, -3.0)
+  );}
+  
+  //extendMatrixExample1: ExtendMatrix := (
+    //baseMatrix: matrixExample1;
+    //rowX: new string[5] ('-x1', '-x2', '-x3', '-x4', '-x5');
+   // columnX: new string[5] ('x6=', 'x7=', 'x8=', 'f ', 'g ');
+  //);
+  
+  matrixExample4 := new real[7, 8] (
+    ( 1.0,  1.0,  1.0,  1.0,  0.0,  0.0,  0.0,  0.0),
+    ( 1.0,  1.0, -2.0,  0.0,  1.0,  0.0,  0.0,  0.0),
+    ( 2.0,  2.0,  3.0,  0.0,  0.0,  1.0,  0.0,  0.0),
+    ( 3.0,  3.0,  2.0,  0.0,  0.0,  0.0,  1.0,  0.0),
+    ( 1.0,  2.0,  2.0,  0.0,  0.0,  0.0,  0.0, -1.0),
+    ( 0.0,  1.0, -1.0,  0.0,  0.0,  0.0,  0.0,  0.0),
+    (-8.0, -9.0, -6.0, -1.0, -1.0, -1.0, -1.0,  1.0)
+  );
+  
+  extendMatrixExample4: ExtendMatrix := (
+    baseMatrix: matrixExample4;
+    rowX: new string[7] ('-x1', '-x2', '-x3', '-x4', '-x5', '-x6', '-x7');
+    columnX: new string[7] ('x8 ', 'x9 ', 'x10 ', 'x11 ', 'x12 ', 'f ', 'g ');
+  );
+  
+procedure printResult();
+begin
+  Writeln('Результат:');
+  Writeln(' ');
+      
+  // исключаем строки g, f и столбец уравнения, поэтому длина - 3
+  var xValues := ArrFill(
+    extendResultMatrix.baseMatrix.GetLength(0) + extendResultMatrix.baseMatrix.GetLength(1) - 3,
+    0.0
+  );
+  
+  // проход по столбцу
+  for var i := 0 to extendResultMatrix.columnX.Length - 3 do
+  begin
+    var variableName := extendResultMatrix.columnX[i];
+    var index := StrToInt(variableName.Substring(1, variableName.Length - 2)) - 1;
+    //var columnXIndex := extendResultMatrix.columnX.IndexOf(variableName);
+    xValues[index] := extendResultMatrix.baseMatrix[i, 0];
+  end;
+  
+  // ответ
+  Write('x  =  ( ');
+  for var i := 0 to xValues.Length - 1 do
+  begin
+    Write(xValues[i]:0:2, ' ');
+  end;
+  Writeln(')');
+  Writeln(' ');
+  
+  // проверка на искуственные переменные 
+  if (xValues.TakeLast(extendResultMatrix.baseMatrix.GetLength(0) - 2).All(x -> x = 0)) then
+  begin
+    var answer := 0.0;
+    for var i := 0 to extendResultMatrix.baseMatrix.ColCount() - 2 do
+      answer += -FuncCoff[i] * xValues[i];
+    Writeln('F(x) = ', answer:0:2)
+  end
+  else        
+    Writeln('Исходная задача не имеет опорного плана');
+end;
+
+begin
+  extendResultMatrix := extendMatrixExample4;
+  FuncCoff := extendResultMatrix.baseMatrix
+    .Row(extendResultMatrix.baseMatrix.GetLength(0) - 2).Skip(1).ToArray();
+  
+  Writeln('Первоначальная матрица:');
+  Writeln('#####################################################################');
+  extendResultMatrix.Print();
+  Writeln(' ');
+  
+  while (true) do
+  begin
+    ColumnIndex := extendResultMatrix.FindColumnIndex();
+    
+    // ищем отрицательный элемент
+    var ColumnElementsNegative := true;
+    for var i := 0 to extendResultMatrix.baseMatrix.GetLength(0) - 3 do
+    begin
+      if (extendResultMatrix.baseMatrix[i, ColumnIndex] > 0) then
+      begin
+        ColumnElementsNegative := false;
+        break;
+      end;
+    end;
+    
+    if (ColumnElementsNegative) then
+    begin
+      Writeln('Нет решений');
+      exit;
+    end;
+    
+    
+    RowIndex := extendResultMatrix.FindRowIndex(ColumnIndex);
+    Writeln('Индекс разрешающего элемента (k, s): (', RowIndex + 1, ', ', ColumnIndex, ')');
+    Writeln(' ');
+    Writeln('Число разрешающий элемент из матрицы: ', extendResultMatrix.baseMatrix[RowIndex, ColumnIndex]);
+    Writeln(' ');
+    
+    tempMatrix := GaussianElimination(extendResultMatrix.baseMatrix, (RowIndex, ColumnIndex));
+    extendResultMatrix.baseMatrix := tempMatrix;
+    extendResultMatrix.ChangeRowAndColumn(RowIndex, ColumnIndex - 1);
+    Writeln('#####################################################################');
+    extendResultMatrix.Print();
+    //Writeln('#################################################################');
+    Writeln(' ');
+    
+    if (extendResultMatrix.RowElementsSuccess(
+        extendResultMatrix.baseMatrix.GetLength(0) - 1))
+    then
+      break;
+    
+    matrixTransformCount += 1;
+  end;
+  
+  while (true) do
+  begin    
+    // поиск индекса столбца, в котором элемент из строки g равен 0, а из строки f меньше 0. begin
+    ColumnIndex := -1;
+    for var i := 1 to extendResultMatrix.baseMatrix.GetLength(1) - 1 do
+      if (
+        (extendResultMatrix.baseMatrix[extendResultMatrix.baseMatrix.GetLength(0) - 1, i] = 0)
+        and (extendResultMatrix.baseMatrix[extendResultMatrix.baseMatrix.GetLength(0) - 2, i] < 0)
+      ) then
+      begin
+        ColumnIndex := i;
+        break;
+      end;
+    
+    
+    if (ColumnIndex > 0) then
+    begin
+      // ищем отрицательный элемент
+      var ColumnElementsNegative := true;
+      for var i := 0 to extendResultMatrix.baseMatrix.GetLength(0) - 3 do
+      begin
+        if (extendResultMatrix.baseMatrix[i, ColumnIndex] > 0) then
+        begin
+          ColumnElementsNegative := false;
+          break;
+        end;
+      end;
+      
+      if (ColumnElementsNegative) then
+      begin
+        Writeln('Нет решений');
+        exit;
+      end;
+      
+      
+      RowIndex := extendResultMatrix.FindRowIndex(ColumnIndex);
+      Writeln('Индекс разрешающего элемента (k, s): (', RowIndex + 1, ', ', ColumnIndex, ')');
+      Writeln(' ');
+      Writeln('Число разрешающий элемент из матрицы: ', extendResultMatrix.baseMatrix[RowIndex, ColumnIndex]);
+      Writeln(' ');
+      
+      tempMatrix := GaussianElimination(extendResultMatrix.baseMatrix, (RowIndex, ColumnIndex));
+      extendResultMatrix.baseMatrix := tempMatrix;
+      extendResultMatrix.ChangeRowAndColumn(RowIndex, ColumnIndex - 1);
+      Writeln('#####################################################################');
+      extendResultMatrix.Print();
+      
+      matrixTransformCount += 1;
+      //Writeln('#################################################################');
+      Writeln(' ');
+    end
+    else
+    begin
+      printResult();      
+      exit;
+    end;
+  end;
+end.
